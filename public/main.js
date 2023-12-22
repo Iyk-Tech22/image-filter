5424a0aa22f1a5c7b698773008d941e3a568cf4a
iyk_faiz <ikechukwugodwin22@yahoo.com>
2023-12-22 13:10:21 +0100

    Compiling rust to wasm and bundling with webpack

--- public/main.js
+++ public/main.js
@@ -1,1 +1,13 @@
-console.log('test');
+( ()=> {
+    const input = document.getElementById("upload");
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+        const base64 = fileReader.result.replace(/^data:image\/(jpe?g|png|gif);base64,/, "");
+        console.log(base64);
+    }
+
+    input.addEventListener("change", () => {
+        fileReader.readAsDataURL(input.files[0]);
+    })
+})()
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
-
+pub fn grayscale(encodeImage: String)
+{
+    
+}
